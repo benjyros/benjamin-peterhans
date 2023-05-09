a6489d90cd89bd1579ea0370a6051d84ce69a98f
benjyros <benny.peterhans@gmail.com>
2023-05-09 21:43:07 +0200

    renamed js to jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Home() {
     return (
         <section className="bg-[#fff5f8] select-none">
+            <p>h</p>
         </section>
     );
 }
