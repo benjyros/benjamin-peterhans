b16bfb94ce5ee2f2542c7ff519f1b6c48cbe54b2
Benjamin Peterhans <111268946+benjyros@users.noreply.github.com>
2023-05-10 11:03:11 +0200

    Benjyros patch 2 (#2)
    * customized light theme
    
    * customized dark theme (provisionally)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,43 +7,26 @@ module.exports = {
     themes: [
       {
         dark: {
-
-          "primary": "#0dff00",
-
-          "secondary": "#f418b6",
-
-          "accent": "#7bf2b8",
-
-          "neutral": "#312541",
-
-          "base-100": "#53395B",
-
-          "info": "#A9CBEA",
-
-          "success": "#1FA88D",
-
-          "warning": "#FCB827",
-
-          "error": "#F3729D",
+          "primary": "#1a202c",
+          "secondary": "#2d3748",
+          "accent": "#9ca3af",
+          "neutral": "#e2e8f0",
+          "base-100": "#4a5568",
+          "info": "#64b5f6",
+          "success": "#48bb78",
+          "warning": "#f6ad55",
+          "error": "#f56565",
         },
         light: {
-          "primary": "#ff6600",
-
-          "secondary": "#f418b6",
-
-          "accent": "#7bf2b8",
-
-          "neutral": "#312541",
-
-          "base-100": "#53395B",
-
-          "info": "#A9CBEA",
-
-          "success": "#1FA88D",
-
-          "warning": "#FCB827",
-
-          "error": "#F3729D",
+          "primary": "#ffffff",
+          "secondary": "#f5f3f2",
+          "accent": "#73737375",
+          "neutral": "#846855",
+          "base-100": "#dbd8d6",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
         },
         cupcake: {
           "primary": "#694229",
